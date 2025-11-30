bbb8211499cd37c24a4e9a084198554ca974904f
Kamalb319 <kamalbashavpr@gmail.com>
2025-11-30 12:12:34 +0530

    basha added few lines

---
+++ src/org/java/Problem.java
@@ -0,0 +1,29 @@
+package org.java;
+
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Vector;
+
+public class Problem {
+	
+	public static void main (String[] args) {
+     
+    String a = "testing";
+    String b = "Selenium";
+    
+    a = a+b;
+    b = a.substring(0, a.length()-b.length());
+    a = a.substring(b.length());
+    
+    System.out.println("afterswapping a:="+a);
+    System.out.println("afteswapping b:="+b);
+    
+    
+		
+	}
+	
+
+}
